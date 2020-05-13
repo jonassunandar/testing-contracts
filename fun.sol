ba23186b26763c0d619eb3ff3e63552b5263d2d1
Dattabot <jonassunandar@yahoo.com>
2020-05-13 12:55:20 +0700

    add bytes32 to string converter

--- fun.sol
+++ fun.sol
@@ -198,4 +198,21 @@ contract fun {
         }
         return ret;
     }
+
+    function bytes32ToString(bytes32 x) public returns (string memory) {
+        bytes memory bytesString = new bytes(32);
+        uint charCount = 0;
+        for (uint j = 0; j < 32; j++) {
+            byte char = byte(bytes32(uint(x) * 2 ** (8 * j)));
+            if (char != 0) {
+                bytesString[charCount] = char;
+                charCount++;
+            }
+        }
+        bytes memory bytesStringTrimmed = new bytes(charCount);
+        for (uint8 j = 0; j < charCount; j++) {
+            bytesStringTrimmed[j] = bytesString[j];
+        }
+        return string(bytesStringTrimmed);
+    }
 }
